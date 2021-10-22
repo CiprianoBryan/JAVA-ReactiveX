c8f4ba8c1488cec6a54988a60d21887c26be0538
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-10-22 00:26:59 -0500

    Creation an Thread

--- src/main/java/com/arimagroup/Main.java
+++ src/main/java/com/arimagroup/Main.java
@@ -5,6 +5,12 @@ import io.reactivex.functions.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-        Observable.just(1, 2, 3, 4, 5).subscribe(System.out::println);
+        /*Observable.just(1, 2, 3, 4, 5).subscribe(System.out::println);*/
+        Thread thread = new Thread(Main::runIt);
+        thread.run();
+    }
+
+    public static void runIt() {
+        System.out.println("Hello World");
     }
 }
