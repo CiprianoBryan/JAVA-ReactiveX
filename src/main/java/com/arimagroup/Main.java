53bbf27b6217fe2bcd93f29aa6760472102a488f
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-10-27 17:58:33 -0500

    Three ways to create Observables

--- src/main/java/com/arimagroup/Main.java
+++ src/main/java/com/arimagroup/Main.java
@@ -1,16 +1,41 @@
 package com.arimagroup;
 
 import io.reactivex.Observable;
-import io.reactivex.functions.Consumer;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        /*Observable.just(1, 2, 3, 4, 5).subscribe(System.out::println);*/
-        Thread thread = new Thread(Main::runIt);
-        thread.run();
+        createObservableWithJust();
+        createObservableFromIterable();
+        createObservableUsingCreate();
     }
 
-    public static void runIt() {
-        System.out.println("Hello World");
+    private static void createObservableWithJust() {
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
+        observable.subscribe(System.out::println);
+    }
+
+    private static void createObservableFromIterable() {
+        List<Integer> list = Arrays.asList(10, 20, 30, 40, 50);
+        Observable<Integer> observable = Observable.fromIterable(list);
+        observable.subscribe(System.out::println);
+    }
+
+    private static void createObservableUsingCreate() {
+        Observable<Integer> observable = Observable.create(observableEmitter -> {
+            observableEmitter.onNext(100);
+            observableEmitter.onNext(200);
+            observableEmitter.onNext(300);
+            observableEmitter.onNext(400);
+            observableEmitter.onNext(500);
+//            observableEmitter.onNext(null); -> Si el emitter empuja un valor null da error
+            observableEmitter.onComplete();
+            observableEmitter.onNext(600); // No se empujará este valor porque ya se lanzó el onComplete()
+        });
+        observable.subscribe(System.out::println,
+                error -> System.out.println("Error: " + error.getLocalizedMessage()),
+                () -> System.out.println("Completed"));
     }
 }
